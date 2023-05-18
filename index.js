640359c65e52fd8dffddb1f1809ec429b0587ca5
Mynul Islam <mynul.sakil@gmail.com>
2023-05-18 12:13:52 +0600

    finished mongo basic set up with environment variable

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT || 5000
@@ -6,6 +6,38 @@ const app = express();
 
 // Use cors middleware to enable CORS
 app.use(cors());
+app.use(express.json()); // Middleware to parse JSON request body
+// for environment variable
+require('dotenv').config()
+
+
+
+
+const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASS}@cluster0.bpciahf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        console.log('mongo Connected Successfully')
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello From Battle Toys server!');
